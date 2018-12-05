cebcb451d13c778548288b0a5b6bc91a517b101e
freshynice <freshynice@live.com>
2018-12-06 00:31:31 +0700

    add comment

--- myprint.js
+++ myprint.js
@@ -10,3 +10,13 @@ function myPrint() {
 
 const printName = myPrint();
 printName(); // Sansanee L.
+
+/****
+Explanation:
+From example above closure is special function inside function again
+name is local variable of myPrint() and function myName() can see and call name to show, then printName is closure 
+
+Pros: Inside function can call all variables of outside function to use no need to copy that variable first, can save memory from this reason.
+
+Cons: For the beginner can get confused on it like me :)
+****/
